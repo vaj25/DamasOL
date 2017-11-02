1f33ae783655b53914b24e907a2a5e93364ce64f
Walter Lemus <walter.lemus@hotmail.com>
2017-11-02 10:53:19 -0600

    no se que hace la dvd

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,9 +13,12 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        $var  = "Hola mundo";
+        
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+            'var'=> $var
         ]);
     }
 }
