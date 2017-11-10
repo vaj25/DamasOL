6c7292f8efec8b2684d9c0b8b42dec97f7460afb
Walter Lemus <walter.lemus@hotmail.com>
2017-11-10 12:51:05 -0600

    Empezando el socket

---
+++ src/AppBundle/Controller/ChatController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class ChatController extends Controller
+{
+    /**
+     * @Route("/chat", name="chat")
+     */
+
+    public function indexAction(Request $request)
+    {
+        $var  = "Hola mundo";
+        
+        // replace this example code with whatever you need
+        return $this->render('default/chat.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+            
+        ]);
+    }
+}
