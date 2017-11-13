b1d8c62ad040344961f7848503ebb04c96f1224b
Alberto S. Castaneda <alb2594@gmail.com>
2017-11-13 16:22:22 -0600

    crear partida de juego

--- src/AppBundle/Controller/GameController.php
+++ src/AppBundle/Controller/GameController.php
@@ -2,6 +2,7 @@
 // src/AppBundle/Controller/GameController.php
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Partida;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -19,7 +20,7 @@ class GameController extends Controller
     }
 
     /**
-     * @Route("/game", name="game")
+     * @Route("/game/{idPartida}", name="game")
      */
     public function gameAction() {
         return $this->render(
@@ -31,8 +32,24 @@ class GameController extends Controller
         );
     }
 
-    public function FunctionName(Type $var = null) {
-        # code...
+    /**
+     * @Route("/partida/new", name="create_partida")
+     */
+    public function nuevaAction() {
+        date_default_timezone_set('America/El_Salvador');
+
+        $partida = new Partida();
+        $partida
+            ->setJugador1($this->getUser()->getId())
+            ->setFecha( new \DateTime() )
+            ->setIsActive(true);
+        
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($partida);
+        $em->flush();
+
+        return $this->redirect('/game/'.$partida->getId());
+
     }
 
 }
