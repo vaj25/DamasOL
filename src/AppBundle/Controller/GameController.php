871073065c9993dbe1697099879c7a42597caca3
Alberto S. Castaneda <alb2594@gmail.com>
2017-11-13 13:21:35 -0600

    agrega pagina de inicio de partida

--- src/AppBundle/Controller/GameController.php
+++ src/AppBundle/Controller/GameController.php
@@ -7,13 +7,24 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class GameController extends Controller
 {
+
+    /**
+     * @Route("/", name="homepage")
+     */
+
+    public function indexAction() {
+        return $this->render(
+            'game/index.html.twig'
+        );
+    }
+
     /**
      * @Route("/game", name="game")
      */
     public function gameAction()
     {
         return $this->render(
-            'game/index.html.twig'
+            'game/game.html.twig'
             // array(
             //     'last_username' => 'hola mundo',
             //     'error'         => '',
